9e9d9f294645d6ff0ec35d6c755715585282e999
Ales Justin <ales.justin@gmail.com>
2019-09-17 16:57:19 +0200

    Fix the test -- so it shows the real problem.

--- independent-projects/arc/tests/src/test/java/io/quarkus/arc/test/producer/generic/GenericProducerHierarchyTest.java
+++ independent-projects/arc/tests/src/test/java/io/quarkus/arc/test/producer/generic/GenericProducerHierarchyTest.java
@@ -1,55 +1,54 @@
 package io.quarkus.arc.test.producer.generic;
 
-import static org.junit.Assert.assertEquals;
-
 import io.quarkus.arc.Arc;
 import io.quarkus.arc.InstanceHandle;
 import io.quarkus.arc.test.ArcTestContainer;
-import java.util.Optional;
-import java.util.function.Function;
-import javax.enterprise.inject.Produces;
-import javax.enterprise.util.TypeLiteral;
-import javax.inject.Singleton;
+import org.junit.Assert;
 import org.junit.Rule;
 import org.junit.Test;
+
+import java.util.Optional;
+import java.util.function.BiConsumer;
+import java.util.function.Function;
+import javax.enterprise.inject.Produces;
+import javax.inject.Inject;
+import javax.inject.Singleton;
 
 public class GenericProducerHierarchyTest {
 
     @Rule
-    public ArcTestContainer container = new ArcTestContainer(Producer.class);
+    public ArcTestContainer container = new ArcTestContainer(Producer.class, Registry.class);
 
-    @SuppressWarnings("serial")
     @Test
     public void testPrimitiveProducers() {
-        InstanceHandle<Function<Optional<String>, String>> strHandle = Arc.container()
-                .instance(new TypeLiteral<Function<Optional<String>, String>>() {
-                });
-        Function<Optional<String>, String> producedStr = strHandle.get();
-        assertEquals("foo", producedStr.apply(Optional.of("FOO")));
-
-        InstanceHandle<Function<Optional<Long>, Long>> longHandle = Arc.container()
-                .instance(new TypeLiteral<Function<Optional<Long>, Long>>() {
-                });
-        Function<Optional<Long>, Long> producedLong = longHandle.get();
-        assertEquals(Long.valueOf(-10), producedLong.apply(Optional.of(10l)));
+        InstanceHandle<Registry> ri = Arc.container().instance(Registry.class);
+        Registry registry = ri.get();
+        Assert.assertNotNull(registry.longProducer.apply((l, s) -> System.out.println(l + s)));
+        Assert.assertNotNull(registry.doubleProducer.apply((l, s) -> System.out.println(l + s)));
     }
 
     @Singleton
     static class Producer {
 
         @Produces
-        public Produced<String, String> produce() {
-            return s -> s.get().toLowerCase();
+        public Produced<Long, String> produceLong() {
+            return c -> Optional.of(10);
         }
 
         @Produces
-        public Produced<Long, Long> produceLong() {
-            return v -> -v.get();
+        public Produced<Double, Long> produceDouble() {
+            return c -> Optional.of(-10);
         }
     }
 
-    static interface Produced<T, R> extends Function<Optional<T>, R> {
-
+    interface Produced<T, R> extends Function<BiConsumer<T, R>, Optional<Integer>> {
     }
 
+    @Singleton
+    static class Registry {
+        @Inject
+        Function<BiConsumer<Long, String>, Optional<Integer>> longProducer;
+        @Inject
+        Function<BiConsumer<Double, String>, Optional<Integer>> doubleProducer;
+    }
 }
