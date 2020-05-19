1d142e190dc5aaffeeb79830ceb752066d7f5cd1
Martin Kouba <mkouba@redhat.com>
2020-05-19 11:47:04 +0200

    InjectMock - minor refactoring

--- test-framework/junit5-mockito/src/main/java/io/quarkus/test/junit/mockito/internal/CreateMockitoSpiesCallback.java
+++ test-framework/junit5-mockito/src/main/java/io/quarkus/test/junit/mockito/internal/CreateMockitoSpiesCallback.java
@@ -1,16 +1,9 @@
 package io.quarkus.test.junit.mockito.internal;
 
-import java.lang.annotation.Annotation;
 import java.lang.reflect.Field;
-import java.util.ArrayList;
-import java.util.List;
-
-import javax.inject.Qualifier;
 
 import org.mockito.Mockito;
 
-import io.quarkus.arc.Arc;
-import io.quarkus.arc.InstanceHandle;
 import io.quarkus.test.junit.callback.QuarkusTestBeforeAllCallback;
 import io.quarkus.test.junit.mockito.InjectSpy;
 
@@ -23,7 +16,7 @@ public class CreateMockitoSpiesCallback implements QuarkusTestBeforeAllCallback 
             for (Field field : current.getDeclaredFields()) {
                 InjectSpy injectSpyAnnotation = field.getAnnotation(InjectSpy.class);
                 if (injectSpyAnnotation != null) {
-                    Object beanInstance = getBeanInstance(testInstance, field);
+                    Object beanInstance = CreateMockitoMocksCallback.getBeanInstance(testInstance, field, InjectSpy.class);
                     Object spy = createSpyAndSetTestField(testInstance, field, beanInstance);
                     MockitoMocksTracker.track(testInstance, spy, beanInstance);
                 }
@@ -43,28 +36,4 @@ public class CreateMockitoSpiesCallback implements QuarkusTestBeforeAllCallback 
         return spy;
     }
 
-    private Object getBeanInstance(Object testInstance, Field field) {
-        Class<?> fieldClass = field.getType();
-        InstanceHandle<?> instance = Arc.container().instance(fieldClass, getQualifiers(field));
-        if (!instance.isAvailable()) {
-            throw new IllegalStateException("Invalid use of @InjectSpy - could not determine bean of type: "
-                    + fieldClass + ". Offending field is " + field.getName() + " of test class "
-                    + testInstance.getClass());
-        }
-        return instance.get();
-    }
-
-    private Annotation[] getQualifiers(Field fieldToSpy) {
-        List<Annotation> qualifiers = new ArrayList<>();
-        Annotation[] fieldAnnotations = fieldToSpy.getDeclaredAnnotations();
-        for (Annotation fieldAnnotation : fieldAnnotations) {
-            for (Annotation annotationOfFieldAnnotation : fieldAnnotation.annotationType().getAnnotations()) {
-                if (annotationOfFieldAnnotation.annotationType().equals(Qualifier.class)) {
-                    qualifiers.add(fieldAnnotation);
-                    break;
-                }
-            }
-        }
-        return qualifiers.toArray(new Annotation[0]);
-    }
 }
