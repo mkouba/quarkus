34326b8a8052e9f53f21f114abf16312a1abd727
Georgios Andrianakis <geoand@gmail.com>
2020-11-06 13:46:25 +0200

    Fix issue with dev console and QuarkusDevModeTest

--- core/deployment/src/main/java/io/quarkus/deployment/util/ArtifactInfoUtil.java
+++ core/deployment/src/main/java/io/quarkus/deployment/util/ArtifactInfoUtil.java
@@ -22,19 +22,25 @@ public final class ArtifactInfoUtil {
      * Returns a Map.Entry containing the groupId and the artifactId of the module the contains the BuildItem
      *
      * The way this works is by depending on the pom.properties file that should be present in the deployment jar
+     * 
      * @return the result, or throws
      */
     public static Map.Entry<String, String> groupIdAndArtifactId(Class<?> clazz) {
         try {
             URL jarLocation = clazz.getProtectionDomain().getCodeSource().getLocation();
-            try (FileSystem fs = FileSystems.newFileSystem(Paths.get(jarLocation.toURI()),
-                    Thread.currentThread().getContextClassLoader())) {
-                Entry<String, String> ret = groupIdAndArtifactId(fs);
-                if(ret == null) {
-                    throw new RuntimeException("Unable to determine groupId and artifactId of the jar that contains "
-                            + clazz.getName() + " because the jar doesn't contain the necessary metadata");
+            if (jarLocation.toString().endsWith(".jar")) {
+                try (FileSystem fs = FileSystems.newFileSystem(Paths.get(jarLocation.toURI()),
+                        Thread.currentThread().getContextClassLoader())) {
+                    Entry<String, String> ret = groupIdAndArtifactId(fs);
+                    if (ret == null) {
+                        throw new RuntimeException("Unable to determine groupId and artifactId of the jar that contains "
+                                + clazz.getName() + " because the jar doesn't contain the necessary metadata");
+                    }
+                    return ret;
                 }
-                return ret;
+            } else {
+                // this is needed only for QuarkusDevModeTest inside Quarkus where the class is read from the corresponding directory
+                return new AbstractMap.SimpleEntry<>("unspecified", "unspecified");
             }
         } catch (IOException | URISyntaxException e) {
             throw new RuntimeException("Unable to determine groupId and artifactId of the jar that contains " + clazz.getName(),
@@ -46,29 +52,29 @@ public final class ArtifactInfoUtil {
      * Returns a Map.Entry containing the groupId and the artifactId of the module the contains the BuildItem
      *
      * The way this works is by depending on the pom.properties file that should be present in the deployment jar
+     * 
      * @return the result, or null if no maven metadata were found
      */
-    public static Map.Entry<String, String> groupIdAndArtifactId(FileSystem fs) {
-        try {
-            Path metaInfPath = fs.getPath("/META-INF");
-            Optional<Path> pomProperties = Files.walk(metaInfPath)
-                    .filter(Files::isRegularFile)
-                    .filter(p -> p.toString().endsWith("pom.properties"))
-                    .findFirst();
-            if (pomProperties.isPresent()) {
-                Properties props = new Properties();
-                props.load(Files.newInputStream(pomProperties.get()));
-                String artifactId = props.getProperty("artifactId");
-                if (artifactId.endsWith(DEPLOYMENT)) {
-                    artifactId = artifactId.substring(0, artifactId.length() - DEPLOYMENT.length());
-                }
-                return new AbstractMap.SimpleEntry<>(props.getProperty("groupId"), artifactId);
-            } else {
-                return null;
+    public static Map.Entry<String, String> groupIdAndArtifactId(FileSystem fs) throws IOException {
+        Path metaInfPath = fs.getPath("/META-INF");
+        return doGroupIdAndArtifactId(metaInfPath);
+    }
+
+    private static AbstractMap.SimpleEntry<String, String> doGroupIdAndArtifactId(Path metaInfPath) throws IOException {
+        Optional<Path> pomProperties = Files.walk(metaInfPath)
+                .filter(Files::isRegularFile)
+                .filter(p -> p.toString().endsWith("pom.properties"))
+                .findFirst();
+        if (pomProperties.isPresent()) {
+            Properties props = new Properties();
+            props.load(Files.newInputStream(pomProperties.get()));
+            String artifactId = props.getProperty("artifactId");
+            if (artifactId.endsWith(DEPLOYMENT)) {
+                artifactId = artifactId.substring(0, artifactId.length() - DEPLOYMENT.length());
             }
-        } catch (IOException x) {
-            throw new RuntimeException("Unable to determine groupId and artifactId of the jar " + fs,
-                                       x);
+            return new AbstractMap.SimpleEntry<>(props.getProperty("groupId"), artifactId);
+        } else {
+            return null;
         }
     }
 }
